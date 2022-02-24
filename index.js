033e5f56475946444ba052b9ba99ff4b01868873
Matthew Senese <matthewjsenese@lewisu.edu>
2022-02-24 11:46:36 -0600

    Added and adjusted files in class

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
-var http = require('http');
-var url = require('url');
+// Implement express to make use with node easier
+const express = require('express')
+const app = express()
 
-const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
+// Directory for files
+app.use(express.static(__dirname + '/public'))
 
-    // Standard Header
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h1>Matthew: Assignment Portfolio</h1><br><br><hr><br>')
+// Port to run on for local host
+const port = process.env.PORT || 8080;
 
-    var q = request.url;
-    if (q != '/past') {
-        response.write('<h2>Current Assignments: </h2><br>')
 
-        response.write('<a href = "https://thankful-pebble-0d8039610.1.azurestaticapps.net/">Getting to Know Our Team</a><br>')
-        response.write('<a href = "https://node-on-azure-mjs.azurewebsites.net/">Node on Azure Example</a><br><br>')
-    }
-    else {
-        response.write('<h2>Past Assignments:</h2><br>')
-            
-        response.write('<a href="https://thankful-water-089720a10.1.azurestaticapps.net/">Web Resume</a> <br>')
-        response.write('<a href="https://kind-hill-02c495910.1.azurestaticapps.net/">Dice Roller</a> <br>')
-        response.write('<a href="https://mango-pebble-032731f10.1.azurestaticapps.net/">Regular Expression Tester</a><br><br>')
-    }
-
-    // Show the url. 
-    response.write("req.url="+request.url+"<br><br>");
-
-    // Suggest adding something to the url so that we can parse it. 
-    response.write("Consider adding '/past' to the URL to see past assignments.<br><br>");
-    
-});
-
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// Listen on the defined port
+// const logMessage = "Server running at http://localhost: " + port
+app.listen(port, () => 
+console.log(`Server running at http://localhost:${port};` 
++ 'press Ctrl-C to terminate.'))
